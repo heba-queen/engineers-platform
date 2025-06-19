e7eb3db04c3a497b589ed2772fa1745a06735729
heba-queen <heba611mansour@gmail.com>
2025-06-20 04:39:50 +0500

    fix env variable

--- backend/engineering/settings.py
+++ backend/engineering/settings.py
@@ -101,7 +101,7 @@ else:
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASE_URL = os.getenv("DATABASE_URL")
+DATABASE_URL = os.getenv("DATABASE_URL_POSTGRE")
 
 DATABASES = {
      "default": dj_database_url.config(default=DATABASE_URL, conn_max_age=1800),
