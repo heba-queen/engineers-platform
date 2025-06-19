612333370e37eeee8b3a5bdcec605160ac64f77f
heba-queen <heba611mansour@gmail.com>
2025-06-20 03:18:04 +0500

    reuse sqlite3

--- backend/engineering/settings.py
+++ backend/engineering/settings.py
@@ -99,12 +99,8 @@ WSGI_APPLICATION = 'engineering.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.environ["PGDATABASE"],
-        'USER': os.environ["PGUSER"],
-        'PASSWORD': os.environ["PGPASSWORD"],
-        'HOST': os.environ["PGHOST"],
-        'PORT': os.environ["PGPORT"],
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
