bac5d3d6fdf44281adace1538a728708b611c71e
heba-queen <heba611mansour@gmail.com>
2025-06-20 05:20:55 +0500

    update urls

--- frontend/src/static/urls.js
+++ frontend/src/static/urls.js
@@ -1,2 +1,2 @@
-export const imageUrl = 'http://127.0.0.1:8000/'
-export const serverUrl = 'http://127.0.0.1:8000/api/'
+export const imageUrl = 'https://engineers-platform-production.up.railway.app/'
+export const serverUrl = 'https://engineers-platform-production.up.railway.app/api/'
